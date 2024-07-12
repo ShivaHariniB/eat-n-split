59a576150090830a26129b6f1b5724feee465445
ShivaHariniB <harinibalakumar02@gmail.com>
2024-07-12 17:16:57 +0530

    FriendsList component built

--- src/App.js
+++ src/App.js
@@ -18,3 +18,45 @@ const initialFriends = [
     balance: 0,
   },
 ];
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />;
+      </div>
+    </div>
+  );
+}
+
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friends friends={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friends({ friends }) {
+  return (
+    <li>
+      <img src={friends.image} alt={friends.name} />
+      <h3>{friends.name}</h3>
+      {friends.balance < 0 && (
+        <p className="red">
+          You owe {friends.name} ${Math.abs(friends.balance)}
+        </p>
+      )}
+      {friends.balance > 0 && (
+        <p className="green">
+          {friends.name} owes you ${Math.abs(friends.balance)}
+        </p>
+      )}
+      {friends.balance === 0 && <p>You and {friends.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
